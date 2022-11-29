6939f6905701dda039cc2b91ff894e5d66232249
RamosFederico <106603607+RamosFederico@users.noreply.github.com>
2022-11-28 21:44:13 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,28 @@
+class Contenedor {[                                                                                                                                                     
+    {                                                                                                                                                    
+      title: 'Lapiz',                                                                                                                                 
+      price: 25,                                                                                                                                     
+      thumbnail: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fsp.depositphotos.com%2Fstock-photos%2Fl%25C3%25A1piz.html&psig=AOvVaw2oteSZpw-CMKxgRv3kH-Rj&ust=1669767630864000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCICun6aP0vsCFQAAAAAdAAAAABAE',                                     
+      id: 11                                                                                                                                              
+    },                                                                                                                                                   
+    {                                                                                                                                                    
+      title: 'Lapicera',                                                                                                                              
+      price: 35,                                                                                                                                     
+      thumbnail: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bikabik.com.ar%2Fproduct%2Flapicera-bic-trazo-medio-azul%2F&psig=AOvVaw18CBdJgETXWC_6SRV29QJE&ust=1669767680835000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOiL5b2P0vsCFQAAAAAdAAAAABAE',                                          
+      id: 12                                                                                                                                              
+    },                                                                                                                                                   
+    {                                                                                                                                                    
+      title: 'Marcador',                                                                                                                          
+      price: 45,                                                                                                                                     
+      thumbnail: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Flibreriaelombu.com%2Fproducto%2Fmarcador-permanente-negro-sharpie%2F&psig=AOvVaw1-7bIznPH_JTe2T7ZT6MGo&ust=1669767710122000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiU58uP0vsCFQAAAAAdAAAAABAE',                                   
+      id: 13                                                                                                                                              
+    }  
+                                                                                                                                      
+  ]}
+
+  save(title): Number -
+  getById(price): Object - 
+  getAll(): Object[] - 
+  deleteById(Number): void -
+  deleteAll(): void  -            
+
